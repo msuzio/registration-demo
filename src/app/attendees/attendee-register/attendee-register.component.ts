31741b6b9606f91cd716deffacbc92cc9ae5aef6
Michael Suzio <msuzio@gmail.com>
2017-10-15 12:28:34 -0400

    Look and feel of registration page is better. Ready to hook up to a model and submit handler.

--- src/app/attendees/attendee-register/attendee-register.component.ts
+++ src/app/attendees/attendee-register/attendee-register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+const states = '["MI","TX","CA"]';
 @Component({
-  selector: 'app-attendee-register',
+  selector: 'attendee-register',
   templateUrl: './attendee-register.component.html',
   styleUrls: ['./attendee-register.component.css']
 })
 export class AttendeeRegisterComponent implements OnInit {
 
+  states: string[];
   constructor() { }
 
   ngOnInit() {
+    this.states = JSON.parse(states);
   }
 
+  onSubmit(event) {
+    console.log("clicked submit" + event);
+  }
 }
