b815ea46d02c4fc9ce1d33a7870edbf5cb688e76
Michael Suzio <msuzio@gmail.com>
2017-10-15 09:30:46 -0400

    got Mongo working, returning to the Angular components

--- src/server.js
+++ src/server.js
@@ -2,72 +2,79 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-//var mongodb = require("mongodb");
-const diskdb = require('diskdb');
-//var assert = require('assert');
-const uuidv1 = require('uuid/v1');
+const mongodb = require("mongodb");
 const dateFormat = require('dateformat');
 
-// var ObjectID = mongodb.ObjectID;
+var ObjectID = mongodb.ObjectID;
 
 
 // our basic trusty tiny web server
 var app = express();
-
-// get the middleware to parse incoming JSON
 app.use(bodyParser.json());
 
-// Connect to the database; using core Mongo support; 
-// Mongoose is a possible option if it helps with server-side validation
-// var mongoURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/registration';
-// mongodb.MongoClient.connect(mongoURL,
-//     function (err, database) {
-//         assert.equal(null, err, "failed to connect to Mongo instance");
-//         if (err) {
-//             console.log(err);
-//             process.exit(1);
-//         }
-//         // database object is deliberate global so it is usuable 
-//         // throughout the application and we don't continuously reconnect.
-//         // Don't like it at all; should wrap this in a service module.
-//         console.log("Database connection ready");
-//         db = database;
-// });
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var mongoURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/registration';
+var db;
+var ObjectID = mongodb.ObjectID;
 
-var db = diskdb.connect('./datastore');
-db.loadCollections(["attendee"]);
+var ATTENDEES_COLLECTION = "attendees";
 
-  // set  up error handling and two endpoints
+mongodb.MongoClient.connect(mongoURL,
+    function (err, database) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        // database object is deliberate global so it is usuable 
+        // throughout the application and we don't continuously reconnect.
+        // Don't like it at all; should wrap this in a service module.
+        console.log("Database connection ready");
+        db = database;
+});
 
-  // Generic error handler used by all endpoints.
+// set  up error handling and two endpoints
+
+// Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
     console.log("ERROR: " + reason);
     res.status(code || 500).json({"error": message});
-  }
-  
-  /*  /event/attendee
+}
+ const ATTENDEE_ENDPOINT = "/event/attendee";
+  /* connect to our attendee endpoint:
   * GET: Return all attendees
   * POST: Registers a new attendee and returns confirmation data
   */
   
-  app.get("/event/attendee", function(req, res) {
-      var all = db.attendees.find();
-      res.status(200).json(all);
+  app.get(ATTENDEE_ENDPOINT, function(req, res) {
+    db.collection(ATTENDEES_COLLECTION).find({}).toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contacts.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
   });
   
-  app.post("/event/attendee", function(req, res) {
-      var attendee = req.body
-      attendee.id = uuidv1();
+  app.post(ATTENDEE_ENDPOINT, function(req, res) {
+      var attendee = req.body;
+      console.log(req.body);
       var now = new Date();
       attendee.registerDate = now;
+      // TODO --  look at better date formatting
       attendee.formattedRegisterDate = dateFormat(now, "UTC:h:MM:ss TT Z");
-      db.attendees.save(attendee);
-      console.log(attendee);
-      res.status(200).json(attendee);
-  });
+      // TODO - validate data
+      db.collection(ATTENDEES_COLLECTION).insertOne(attendee, function(err, doc) {
+        if (err) {
+          handleError(res, err.message, "Failed to create new attendee.");
+        } else {
+          res.status(201).json(doc.ops[0]);
+        }
+      }
+  );
+});
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+ });
