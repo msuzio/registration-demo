b2eb83e8143e34f5d835cad71224146fbff11a3d
Michael Suzio <msuzio@gmail.com>
2017-10-16 15:18:53 -0400

    uodated misc. test files

--- src/test.js
+++ src/test.js
@@ -1,13 +1,46 @@
  'use strict'
+
+ function compare (a,b) {
+  if ((a === undefined || a === null) || (b === undefined || b === null)) {
+    // do something 
+    console.log("a =" +a +" b ="+ b);
+}
+    if (a) {
+      if (b) {
+      if (a == b) {
+        return 0;
+        } else  {
+          return a < b ? 1:-1;
+        }
+      } else {
+        // no b -- sort lower
+        return -1;
+      }
+    } else {
+    // no a -- sort a lower
+    return -1;
+  }
+}
+
  var dates = [];
- for (let i = 0; i <4; i++) {
+ for (let i = 0; i <10; i++) {
   let  d = new Date();
-  d.setMinutes(i);
-  dates.unshift(d);
+  if (i %2) {
+    d.setMinutes(i);
+  } else {
+    d.setHours(12-i);
+    dates.unshift(d);
  }
+}
  dates.unshift(null);
  dates.push(undefined);
+ dates.unshift(null);
+ dates.unshift(undefined);
 
+ for (let j = 0; j <dates.length; j++) {
+  console.log(dates[j]);
+}
+console.log("Sorted");
 dates.sort(compare);
 for (let j = 0; j <dates.length; j++) {
   console.log(dates[j]);
